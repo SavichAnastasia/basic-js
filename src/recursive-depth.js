9afb4a181c66d9eff2d542fd6cc4759c9adbfff4
Anastasia Savich <anastasia.savich.s@gmail.com>
2020-02-24 14:42:20 +0300

    feat: add recrusive-depth task solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,21 +1,11 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr) {
-        throw 'Not implemented';
-        // let counter = [];
-
-        // function rec(mass) {
-        //     let count = 1;
-        //     mass.forEach(item => {
-        //         if (Array.isArray(item)) {
-        //             count++;
-        //             return rec(item);
-        //         }
-                
-        //     })
-        //     counter.push(count)
-        // }
-
-        // rec(arr);
-        // return Math.max(...counter);
+    calculateDepth(arr, count = 0, counter = []) {
+        counter.push(++count);
+        
+        arr.forEach(item => {
+            if (Array.isArray(item)) this.calculateDepth(item, count, counter)
+        })
+       
+        return Math.max(...counter);
     }
 };
